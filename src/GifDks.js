79cbbd9d8feb19ca963174eb824f1b227fa18e44
Danniels Castillo <danniels.magana@gmail.com>
2021-07-15 21:57:04 -0600

    Creando e Inicializando Componente CategoryAdd

--- src/GifDks.js
+++ src/GifDks.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {CategoryAdd} from "./components/CategoryAdd";
 
 export const GifDks = () => {
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+    // const handleAdd = () => {
+        // setCategories([...categories, 'HunterXHunter']);
+        // setCategories(cats => [...cats, 'HunterXHunter']);
+    // }
     return(
         <>
             <h2>GifDKS</h2>
+            <CategoryAdd setCategories={setCategories}/>
             <hr />
+            {/*<button onClick={handleAdd}>Add(+)</button>*/}
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={category}> {category} </li>
+                    })
+                }
+            </ol>
         </>
     )
 }
